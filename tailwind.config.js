91957d9c075ae1917ff61deb3102bc13882a017a
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-29 10:08:56 -0400

    Add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      colors: {
+        // The following colours deviate from the tailwind default
+        gray: colors.gray,
+        green: colors.emerald,
+        blue: colors.lightBlue,
+        purple: colors.fuchsia,
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}
