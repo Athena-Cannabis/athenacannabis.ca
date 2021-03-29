cc9f747426bd7f68867679c57df9fb04f5a45215
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-29 10:49:54 -0400

    Rename home view conponent

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
+import Home from '../views/Home.vue';
 
 const NotFound = () => import('../views/NotFound.vue');
 
@@ -9,7 +9,7 @@ export const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomePage,
+      component: Home,
     },
     {
       path: '/404',
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+    <home-hero />
+    <home-about />
+    <home-team />
+    <home-contact />
+
+  </div>
+</template>
+
+<script>
+import HomeHero from '../components/Home/HomeHero.vue';
+import HomeAbout from '../components/Home/HomeAbout.vue';
+import HomeTeam from '../components/Home/HomeTeam.vue';
+import HomeContact from '../components/Home/HomeContact.vue';
+
+
+export default {
+  components : {
+    HomeHero,
+    HomeAbout,
+    HomeTeam,
+    HomeContact,
+  },
+  setup() {
+
+  }
+}
+</script>
