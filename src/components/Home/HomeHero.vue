78621558b31439ed5b0c86c7e2a25b32e132c550
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-01 14:56:13 -0400

    Add sample hero

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <p>The hero</p>
+  <div class="bg-white">
+    <div class="container">
+      <div class="my-20 flex flex-col md:flex-row">
+
+        <div class="w-full lg:w-3/5">
+
+          <div class="my-20">
+            <div>
+              <div class="inline-flex space-x-4">
+                <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
+                  Opening soon!
+                </span>
+              </div>
+            </div>
+            <div class="mt-6 sm:max-w-xl">
+              <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-6xl lg:text-8xl">
+                Server management for growing teams
+              </h1>
+              <p class="mt-6 text-xl text-gray-500">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="w-full lg:w-2/5 flex items-center">
+           <img
+            class="object-cover h-1/2 w-full md:transform md:scale-110 md:rotate-3 rounded-3xl shadow-xl"
+            src="/images/home/hero.jpg" alt="">
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
