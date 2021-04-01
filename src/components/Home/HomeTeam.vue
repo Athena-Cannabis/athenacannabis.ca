8967a4f5a3a6ac53bf0efcacb7a879669de804cb
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-01 15:30:50 -0400

    Add team section

--- src/components/Home/HomeTeam.vue
+++ src/components/Home/HomeTeam.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <p>The Team</p>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="bg-brand-blue-700 my-32">
+  <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+    <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+      Join Our Team - We’re hiring!
+    </h2>
+    <p class="mt-4 text-lg leading-6 text-indigo-200">If you’re passionate about customer service and providing the community with a safe and accessible way to purchase cannabis, we’d love to hear from you!</p>
+
+    <p class="mt-4 text-lg leading-6 text-indigo-200">At this time, we are hiring for the positions of part time Budtenders. Applicant is responsible for obtaining a Criminal Record Check and Cannsell Certificate, prior to their first scheduled shift but not necessary to apply. </p>
+
+    <p class="mt-4 text-lg leading-6 text-indigo-200">Please send an email with your updated resume to <a class="font-medium text-white underline" href="mailto:hello@athenacannabis.ca">hello@athenacannabis.ca</a></p>
+
+
+
+
+
   </div>
+</div>
+
 </template>
