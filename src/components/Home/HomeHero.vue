5394f958e8062acfe0c94b45244138354fcf07c5
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-01 15:13:04 -0400

    Add text

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-white">
     <div class="container">
-      <div class="my-20 flex flex-col md:flex-row">
+      <div class="my-10 flex flex-col md:flex-row">
 
         <div class="w-full lg:w-3/5">
 
-          <div class="my-20">
+          <div class="my-10">
             <div>
               <div class="inline-flex space-x-4">
                 <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
@@ -15,11 +15,13 @@
             </div>
             <div class="mt-6 sm:max-w-xl">
               <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-6xl lg:text-8xl">
-                Server management for growing teams
+                An inviting, legal and judgement-free cannabis retail experience in Kanata South
               </h1>
+              <!--
               <p class="mt-6 text-xl text-gray-500">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
               </p>
+              -->
             </div>
           </div>
 
