9a62287406e9ec5dc3b1ce6844bfb1265742f813
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-01 15:26:00 -0400

    Intial about section

--- src/components/Home/HomeAbout.vue
+++ src/components/Home/HomeAbout.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <p>The about</p>
+  <div class="bg-white">
+    <div class="container">
+      <div class="text-center">
+        <p class="uppercase text-brand-blue-700 font-bold tracking-widest mb-5">Our Approach</p>
+        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+          Cannabis Uncomplicated
+        </h2>
+        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+          We go above and beyond customer service expectations, helping you select the right product for your wants and needs, and being committed to protecting public health and safety.
+        </p>
+    </div>
+    </div>
   </div>
 </template>
