453d87448d8f0e8f6c01b28fdb25a49aae87ea8c
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-03 18:12:56 -0400

    Remove the second placeholder

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -25,74 +25,12 @@
           </div>
           <div class="w-full md:w-3/5 flex items-center">
            <img
-            class="object-cover object-left objecst-right-top h-full rounded-2xl"
+            class="object-cover object-left objecst-right-top h-full rounded-3xl shadow-l"
             src="/images/home/hero.jpg" alt="">
           </div>
         </div>
 
       </div>
     </section>
-  </div>
-  <div class="bg-white">
-    <div class="container">
-      <div class="my-32 lg:pl-20">
-
-        <div>
-          <div class="inline-flex space-x-4">
-            <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
-              Coming soon!
-            </span>
-          </div>
-        </div>
-        <div class="mt-6 xl:w-4/5 ">
-          <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-6xl lg:text-7xl">
-            An inviting, legal and judgement-free cannabis retail experience in Kanata South
-          </h1>
-          <!--
-          <p class="mt-6 text-xl text-gray-500">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-          </p>
-          -->
-        </div>
-      </div>
-
-    </div>
-  </div>
-  <div v-if="false" class="bg-white">
-    <div class="container">
-      <div class="my-10 flex flex-col md:flex-row">
-
-        <div class="w-full lg:w-3/5">
-
-          <div class="my-10">
-            <div>
-              <div class="inline-flex space-x-4">
-                <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
-                  Opening soon!
-                </span>
-              </div>
-            </div>
-            <div class="mt-6 sm:max-w-xl">
-              <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-6xl lg:text-8xl">
-                An inviting, legal and judgement-free cannabis retail experience in Kanata South
-              </h1>
-              <!--
-              <p class="mt-6 text-xl text-gray-500">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-              </p>
-              -->
-            </div>
-          </div>
-
-        </div>
-
-        <div class="w-full lg:w-2/5 flex items-center">
-           <img
-            class="object-cover h-1/2 w-full md:transform md:scale-110 md:rotate-3 rounded-3xl shadow-xl"
-            src="/images/home/hero.jpg" alt="">
-        </div>
-
-      </div>
-    </div>
   </div>
 </template>
