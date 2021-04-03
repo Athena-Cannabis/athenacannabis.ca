b697db972f4b2d081ae14f04bd7b7a10b622eb8a
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-03 18:25:06 -0400

    Update styles

--- src/components/Home/HomeTeam.vue
+++ src/components/Home/HomeTeam.vue
@@ -1,21 +1,24 @@
 <template>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-brand-blue-700 my-32">
-  <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-    <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
-      Join Our Team - We’re hiring!
-    </h2>
-    <p class="mt-4 text-lg leading-6 text-indigo-200">If you’re passionate about customer service and providing the community with a safe and accessible way to purchase cannabis, we’d love to hear from you!</p>
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+      <div class="bg-brand-blue-800 rounded-2xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+        <div class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+          <div class="lg:self-center">
+            <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+              <span class="block">Join Our Team</span>
+              <span class="block">We’re hiring!</span>
+            </h2>
+            <p class="mt-4 text-lg leading-6 text-brand-blue-50">If you’re passionate about customer service and providing the community with a safe and accessible way to purchase cannabis, we’d love to hear from you!</p>
 
-    <p class="mt-4 text-lg leading-6 text-indigo-200">At this time, we are hiring for the positions of part time Budtenders. Applicant is responsible for obtaining a Criminal Record Check and Cannsell Certificate, prior to their first scheduled shift but not necessary to apply. </p>
+            <p class="mt-4 text-lg leading-6 text-brand-blue-50">At this time, we are hiring for the positions of part time Budtenders. Applicant is responsible for obtaining a Criminal Record Check and Cannsell Certificate, prior to their first scheduled shift but not necessary to apply. </p>
 
-    <p class="mt-4 text-lg leading-6 text-indigo-200">Please send an email with your updated resume to <a class="font-medium text-white underline" href="mailto:hello@athenacannabis.ca">hello@athenacannabis.ca</a></p>
-
-
-
-
-
+            <p class="mt-4 text-lg leading-6 text-brand-blue-50">Please send an email with your updated resume to <a class="font-medium text-white underline" href="mailto:hello@athenacannabis.ca">hello@athenacannabis.ca</a></p>
+          </div>
+        </div>
+        <div class="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
+          <img class="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20" src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg" alt="App screenshot">
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-
 </template>
