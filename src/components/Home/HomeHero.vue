3bdabdfd51c375274a40bb83edc6abe942abffb2
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-04 10:33:44 -0400

    Fix spacing and container styles

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
     <section>
-      <h2 class="sr-only">Athena Cannabis Main Hero Section</h2>
-      <div class="px-5 lg:px-20  py-10 bg-gray-50">
-        <div class="my-10 flex flex-col md:flex-row">
-          <div class="w-full lg:w-2/5 pb-52">
-            <!--
-            <div>
-              <div class="inline-flex space-x-4">
-                <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
-                  Opening soon!
-                </span>
-              </div>
-            </div>
-            -->
-            <h3 class="text-4xl font-bold text-gray-900 tracking-tight sm:text-6xl lg:text-7xl">
-              <span class="block">Opening </span>
-              <span class="block">mid April</span>
-            </h3>
-            <p class="mt-5 text-5xl text-gray-500 w-3/4">
-              An inviting, legal and judgement-free cannabis retail experience coming soon to
-              Kanata South
-            </p>
-          </div>
-          <div class="w-full md:w-3/5 flex items-center">
-           <img
-            class="object-cover object-left objecst-right-top h-full rounded-3xl shadow-l"
-            src="/images/home/hero.jpg" alt="">
-          </div>
-        </div>
+      <div class="bg-gray-50">
+        <div class="py-10 px-5 2xl:px-8 2xl:container">
 
+          <h2 class="sr-only">Athena Cannabis Main Hero Section</h2>
+          <div class="my-10 flex flex-col md:flex-row">
+            <div class="w-full lg:w-2/5 pb-52">
+              <!--
+              <div>
+                <div class="inline-flex space-x-4">
+                  <span class="rounded bg-brand-blue-50 px-2.5 py-1 text-xs font-semibold text-brand-blue-700 tracking-wide uppercase">
+                    Opening soon!
+                  </span>
+                </div>
+              </div>
+              -->
+              <h3 class="text-4xl font-bold text-gray-900 tracking-tight sm:text-6xl lg:text-7xl">
+                <span class="block">Opening </span>
+                <span class="block">mid April</span>
+              </h3>
+              <p class="mt-5 text-5xl text-gray-500 w-3/4">
+                An inviting, legal and judgement-free cannabis retail experience coming soon to
+                Kanata South
+              </p>
+            </div>
+            <div class="w-full md:w-3/5 flex items-center">
+            <img
+              class="object-cover object-left objecst-right-top h-full rounded-3xl shadow-l"
+              src="/images/home/hero.jpg" alt="">
+            </div>
+          </div>
+
+        </div>
       </div>
     </section>
   </div>
