419f843457ed0fae043622c0cfee6587d2e513cb
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-03 23:35:56 -0400

    Add Gallery

--- src/components/Home/HomeGallery.vue
+++ src/components/Home/HomeGallery.vue
@@ -1,32 +1,34 @@
 <template>
- <div>
-   <section>
-     <div class="px-10 py-20 bg-gray-50">
-       <div class="max-w-7xl mx-auto">
+  <div>
+    <section>
+      <div class="bg-gray-50">
+        <div class="pl-20 pr-10 pt-20 pb-32  bg-gray-50">
+          <div class="w-full">
 
-         <!-- Text section -->
-         <div class="text-left">
-          <h2 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
-            What we're building
-          </h2>
-          <p class="mt-3 max-w-lg text-xl text-gray-500 sm:mt-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
-          </p>
+            <!-- Text section -->
+            <div class="text-left">
+              <h2 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
+                What we're building
+              </h2>
+              <p class="mt-3 max-w-lg text-xl text-gray-500 sm:mt-4">
+                We can't show you the store just yet, but we're excited to share with you some pre-construction renderings from our design phase.
+              </p>
+            </div>
+
+            <!-- Images section -->
+            <div class="mt-12 max-w-lg mx-auto grid gap-10 lg:grid-cols-3 lg:max-w-none">
+
+              <img class="h-80 w-full object-cover rounded-xl shadow-xl" src="/images/home/gallery-a.jpg" alt="">
+
+              <img class="h-80 w-full object-cover rounded-xl shadow-xl" src="/images/home/gallery-b.jpg" alt="">
+
+              <img class="h-80 w-full object-cover object-left rounded-xl " src="/images/home/gallery-c.jpg" alt="">
+
+            </div>
+
+          </div>
         </div>
-
-        <!-- Images section -->
-        <div class="mt-12 max-w-lg mx-auto grid gap-10 lg:grid-cols-3 lg:max-w-none">
-
-          <img class="h-48 w-full object-cover rounded-xl " src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=ocVTDFghBA&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
-
-          <img class="h-48 w-full object-cover rounded-xl " src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=ocVTDFghBA&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
-
-          <img class="h-48 w-full object-cover rounded-xl " src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=ocVTDFghBA&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
-
-        </div>
-
-       </div>
-     </div>
-   </section>
- </div>
+      </div>
+    </section>
+  </div>
 </template>
