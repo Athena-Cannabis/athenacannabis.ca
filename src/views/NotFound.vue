66e6ccf84819fbb4bfc326e3d247260b4dcee565
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-06 15:10:19 -0400

    Add some styling

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <p>Not found</p>
+    <section>
+      <div class="bg-gray-50">
+        <div class="py-10 px-5 2xl:px-8 2xl:container">
+
+          <h1>404</h1>
+          <p>Page not found</p>
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
