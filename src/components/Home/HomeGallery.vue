b4694bad4e3d959bfa70b8fdaa42ce5d6362c567
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-08 16:23:38 -0400

    Update gallery text

--- src/components/Home/HomeGallery.vue
+++ src/components/Home/HomeGallery.vue
@@ -7,19 +7,19 @@
             <!-- Text section -->
             <div class="text-left max-w-prose">
               <h2 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
-                What we're building
+                Check out the store
               </h2>
 
               <p class="mt-3 text-xl text-gray-500 sm:mt-4">
-                The store is going to feature an elegant and inviting hotel inspired lobby which connects to the age restricted retail space.
+                Welcome to Athena Cannabis. We cannot wait for you to check out the exciting space that we have created for you.
               </p>
 
               <p class="mt-3 text-xl text-gray-500 sm:mt-4">
-                Throughout the store, we are showcasing lots of warm light, natural materials, understated aesthetics and some innovative ideas that we are sure will create a welcoming customer experience.
+                Throughout the store, we have showcased lots of warm light, natural materials, understated aesthetics and some innovative ideas that we are sure will create a welcoming customer experience.
               </p>
 
               <p class="mt-3 text-xl text-gray-500 sm:mt-4">
-                We can't invite you into the store just yet, but we're excited to share some pre-construction renderings from our design phase.
+                As we're in lockdown, we can't invite you into the store just yet, but we're excited to share some pictures now that construction is complete.
               </p>
             </div>
 
