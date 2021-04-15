3f00f38cd5f708b810362fda9f1ab038509700ea
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-15 10:55:01 -0400

    Update gap styles

--- src/components/Home/HomeCurbside.vue
+++ src/components/Home/HomeCurbside.vue
@@ -7,7 +7,7 @@
             <!-- Text section -->
             <div class="text-left max-w-prose">
               <h2 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
-                Curbside pickup: here's how it works!
+                Curbside pickup
               </h2>
 
               <p class="mt-3 text-xl text-gray-500 sm:mt-8">
@@ -38,61 +38,6 @@
 
             </div>
 
-            <!-- Steps section -->
-            <ol class="list-none mt-12 grid gap-2 md:gap-8 md:grid-cols-2 lg:grid-cols-4">
-
-              <!-- Step 1 -->
-              <li
-                class="">
-
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
-                <h2 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 1</h2>
-                <p class="text-lg text-gray-900 font-medium title-font mb-4">
-                  <span class="sr-only">Title: </span>Browse our menu</p>
-                <p class="text-lg text-gray-600">Visit our <a rel="external" class="text-brand-blue-700 font-medium underline hover:text-brand-blue-900 transition-all ease-in-out duration-150" href="https://athenawebmenu.azurewebsites.net/">online menu</a>. Browse through a host of different products like dried flower, pre-rolls, edibles, cannabis infused beverages and much more.</p>
-
-              </li>
-
-              <!-- Step 2 -->
-              <li
-                class="">
-
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
-                <h2 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 2</h2>
-                <p class="text-xl text-gray-900 font-medium mb-4">
-                  <span class="sr-only">Title: </span>Checkout and payment</p>
-                <p class="text-lg text-gray-600">Need help navigating the store? Feeling overwhelmed with all the choices? Call the store at <a href="tel:+15551000">613-555-1000</a> and our knowledable staff will be able to help you.</p>
-
-              </li>
-
-              <!-- Step 3 -->
-              <li
-                class="">
-
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
-                <h2 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 3</h2>
-                <p class="text-xl text-gray-900 font-medium mb-4">
-                  <span class="sr-only">Title: </span>Receive confirmation email</p>
-                <p class="text-lg text-gray-600">Receive an email confirmation and pickup details when your order is ready. Orders are usualy ready in as little as 1 hour.</p>
-
-              </li>
-
-              <!-- Step 4 -->
-              <li
-                class="">
-
-                <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
-                <h2 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 4</h2>
-                <p class="text-xl text-gray-900 font-medium mb-4">
-                  <span class="sr-only">Title: </span>Head to the store</p>
-                <p class="text-lg text-gray-600">When you get to the store, please make sure that you have your valid ID and credit card that the purchase was paid with.</p>
-              </li>
-
-
-
-            </ol>
-
-
             <!-- Images section
             <div class="mt-12 grid gap-10 lg:grid-cols-3 lg:max-w-none">
 
@@ -104,6 +49,84 @@
 
             </div>
             -->
+
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="bg-gray-50">
+        <div class="py-20 px-4 sm:px-5 2xl:px-8 2xl:container">
+
+            <!-- Text section -->
+            <div class="text-left max-w-prose">
+              <h3 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
+                Here's how it works
+              </h3>
+
+            </div>
+
+            <div class="">
+
+              <!-- Steps section -->
+              <ol class="list-none mt-12 grid gap-2 md:gap-28 md:grid-cols-2 lg:grid-cols-4">
+
+                <!-- Step 1 -->
+                <li
+                  class="">
+
+                  <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                  <h4 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 1</h4>
+                  <p class="text-lg text-gray-900 font-medium title-font mb-4">
+                    <span class="sr-only">Title: </span>Browse our menu</p>
+                  <p class="text-lg text-gray-600">Visit our <a rel="external" class="text-brand-blue-700 font-medium underline hover:text-brand-blue-900 transition-all ease-in-out duration-150" href="https://athenawebmenu.azurewebsites.net/">online menu</a>. Browse through a host of different products like dried flower, pre-rolls, edibles and much more.</p>
+
+                </li>
+
+                <!-- Step 2 -->
+                <li
+                  class="">
+
+                  <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                  <h4 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 2</h4>
+                  <p class="text-xl text-gray-900 font-medium mb-4">
+                    <span class="sr-only">Title: </span>Checkout and payment</p>
+                  <p class="text-lg text-gray-600">Need help navigating the online menu? Call the store at <a href="tel:+15551000">613-555-1000</a> for help from our knowledable staff.</p>
+
+                </li>
+
+                <!-- Step 3 -->
+                <li
+                  class="">
+
+                  <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                  <h4 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 3</h4>
+                  <p class="text-xl text-gray-900 font-medium mb-4">
+                    <span class="sr-only">Title: </span>Receive confirmation email</p>
+                  <p class="text-lg text-gray-600">Receive an email confirmation and pickup details when your order is ready. Orders are usualy ready in as little as 1 hour.</p>
+
+                </li>
+
+                <!-- Step 4 -->
+                <li
+                  class="">
+
+                  <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+                  <h4 class="tracking-widest text-brand-blue-500 text-sm font-medium uppercase">Step 4</h4>
+                  <p class="text-xl text-gray-900 font-medium mb-4">
+                    <span class="sr-only">Title: </span>Head to the store</p>
+                  <p class="text-lg text-gray-600 mb-4"><a
+                      href="https://goo.gl/maps/rG3U2kfMMjundn8P6"
+                      class=" text-brand-blue-700 font-medium text-lg underline hover:text-brand-blue-900
+                            transition-all ease-in-out duration-150">
+                      Need directions?</a> When you arrive, please make sure that you have a valid ID and the credit card that the purchase was paid with.</p>
+                  <p class="mt-4">
+
+                  </p>
+                </li>
+
+              </ol>
+
+            </div>
 
         </div>
       </div>
