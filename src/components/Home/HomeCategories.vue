c0cfc76ac08db72f9c1bc8e7b7eb0475ee73308f
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-04-20 16:13:42 -0400

    Add SVG to category model

--- src/components/Home/HomeCategories.vue
+++ src/components/Home/HomeCategories.vue
@@ -18,7 +18,7 @@
             <div class="mt-10">
 
                 <ul
-                  class="list-none grid grid-cols-3 lg:grid-cols-5 gap-4">
+                  class="list-none grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
 
                   <li
                     v-for="category in productsCategories"
@@ -27,26 +27,41 @@
 
                       <router-link
                         :to="{ name: 'category-products', params: { slug: category.slug }}"
-                        class="flex flex-col">
+                        class="flex flex-col items-center
+                              border-2 border-blue-900 rounded-2xl
+                              p-5 h-full hover:shadow-2xl">
 
-                        <div class="order-2 text-center mb-16">
+                        <!-- Title and description -->
+                        <div class="order-2 text-left mt-5 w-full">
 
                           <h3
-                            class="text-center tracking-wide text-xl text-gray-800 font-bold">
+                            class="text-left text-xl text-brand-blue-800 font-bold">
                             {{ category.title }}
                           </h3>
 
-                          <p class="text-gray-600">{{ category.description }}</p>
+                          <p class="hidden md:block text-gray-600">{{ category.description }}</p>
 
                         </div>
 
-                        <div class="order-1">
-                          <img
-                            class="my-5 h-48 w-full object-contain object-center rounded-lg"
-                            :src="category.image.src"
-                            :alt="category.image.alt" />
+                        <!-- SVG Icon if it exists -->
+                        <div
+                          v-if="category.icon"
+                          class="order-1"
+                        >
+
+                          <svg class="h-12 fill-current text-brand-blue-700" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" :view-box.camel="category.icon.viewbox" xml:space="preserve" v-html="category.icon.contents"></svg>
 
                         </div>
+                        <div
+                          v-else
+                          class="order-1">
+
+                          <!-- Placeholder SVG -->
+                          <svg class=" h-12 fill-current text-brand-blue-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" x="0px" y="0px"><title>Artboard 34</title><g data-name="Layer 2"><path d="M4.33,15h7.34A3.34,3.34,0,0,0,15,11.67V4.33A3.34,3.34,0,0,0,11.67,1H4.33A3.34,3.34,0,0,0,1,4.33v7.34A3.34,3.34,0,0,0,4.33,15ZM3,4.33A1.34,1.34,0,0,1,4.33,3h7.34A1.34,1.34,0,0,1,13,4.33v7.34A1.34,1.34,0,0,1,11.67,13H4.33A1.34,1.34,0,0,1,3,11.67Z"></path><path d="M27.67,1H20.33A3.34,3.34,0,0,0,17,4.33v7.34A3.34,3.34,0,0,0,20.33,15h7.34A3.34,3.34,0,0,0,31,11.67V4.33A3.34,3.34,0,0,0,27.67,1ZM29,11.67A1.34,1.34,0,0,1,27.67,13H20.33A1.34,1.34,0,0,1,19,11.67V4.33A1.34,1.34,0,0,1,20.33,3h7.34A1.34,1.34,0,0,1,29,4.33Z"></path><path d="M4.33,31h7.34A3.34,3.34,0,0,0,15,27.67V20.33A3.34,3.34,0,0,0,11.67,17H4.33A3.34,3.34,0,0,0,1,20.33v7.34A3.34,3.34,0,0,0,4.33,31ZM3,20.33A1.34,1.34,0,0,1,4.33,19h7.34A1.34,1.34,0,0,1,13,20.33v7.34A1.34,1.34,0,0,1,11.67,29H4.33A1.34,1.34,0,0,1,3,27.67Z"></path><path d="M24,17a7,7,0,1,0,7,7A7,7,0,0,0,24,17Zm0,12a5,5,0,1,1,5-5A5,5,0,0,1,24,29Z"></path></g></svg>
+
+                        </div>
+
+
 
 
                       </router-link>
