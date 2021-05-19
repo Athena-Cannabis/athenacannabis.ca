1095a97c97b80edf0aa91163ce2aea45132b1e9e
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-05-19 15:25:23 -0400

    Add gallery redesign attempt

--- src/components/Home/HomeGallery.vue
+++ src/components/Home/HomeGallery.vue
@@ -5,79 +5,68 @@
       <!-- Content container -->
       <div class="py-5 md:py-10 px-4 md:px-12 xl:px-20 2xl:container">
 
-        <!-- Text section -->
-        <div class="text-left max-w-prose">
+        <div class="grid grid-cols-2 gap-8">
 
-          <h2 class="text-3xl sm:text-2xl lg:text-4xl xl:text-5xl text-brand-blue-900">
-            What we've built
-          </h2>
+          <!-- Text Box -->
+          <div class="row-span-1 p-10">
 
-          <p class="mt-3 sm:mt-8 text-xl text-gray-500">
-            Due to the provincial lockdown we can't invite you into the store, but we're excited to share some pre-construction renderings from our design phase.
-          </p>
-
-          <p class="mt-3 sm:mt-4 text-xl text-gray-500">
-            The store features an elegant and inviting hotel inspired lobby which connects to the age restricted retail space.
-          </p>
-
-          <p class="mt-3 sm:mt-4 text-xl text-gray-500">
-            Throughout the store, we are showcasing lots of warm light, natural materials, understated aesthetics and some innovative ideas that we are sure will create a welcoming customer experience.
-          </p>
-
-        </div>
-
-        <!-- Images section -->
-        <div class="mt-12 grid gap-10 lg:grid-cols-3 lg:max-w-none">
-
-          <img class="h-36 md:h-72 w-full object-cover rounded-lg shadow-xl" src="/images/home/gallery-a.jpg" alt="">
-
-          <img class="h-36 md:h-72 w-full object-cover rounded-lg shadow-xl" src="/images/home/gallery-b.jpg" alt="">
-
-          <img class="h-36 md:h-72 w-full object-cover object-left rounded-lg shadow-xl" src="/images/home/gallery-c.jpg" alt="">
-
-        </div>
-
-      </div>
-    </div>
-  </section>
-  <div v-if="false">
-    <section>
-      <div class="bg-gray-50">
-        <div class="py-20 px-4 sm:px-5 2xl:px-8 2xl:container">
-
-            <!-- Text section -->
             <div class="text-left max-w-prose">
-              <h2 class="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl">
-                What we've built
+
+              <h2 class="text-3xl sm:text-2xl lg:text-4xl xl:text-5xl text-gray-800 font-semibold">
+                 What we've built
               </h2>
 
-              <p class="mt-3 text-xl text-gray-500 sm:mt-4">
+              <p class="mt-3 sm:mt-8 text-xl text-gray-500">
                 Due to the provincial lockdown we can't invite you into the store, but we're excited to share some pre-construction renderings from our design phase.
               </p>
 
-              <p class="mt-3 text-xl text-gray-500 sm:mt-4">
+              <p class="mt-3 sm:mt-4 text-xl text-gray-500">
                 The store features an elegant and inviting hotel inspired lobby which connects to the age restricted retail space.
               </p>
 
-              <p class="mt-3 text-xl text-gray-500 sm:mt-4">
+              <p class="mt-3 sm:mt-4 text-xl text-gray-500">
                 Throughout the store, we are showcasing lots of warm light, natural materials, understated aesthetics and some innovative ideas that we are sure will create a welcoming customer experience.
               </p>
 
             </div>
 
-            <!-- Images section -->
-            <div class="mt-12 grid gap-10 lg:grid-cols-3 lg:max-w-none">
 
-              <img class="h-36 md:h-72 w-full object-cover rounded-lg shadow-xl" src="/images/home/gallery-a.jpg" alt="">
 
-              <img class="h-36 md:h-72 w-full object-cover rounded-lg shadow-xl" src="/images/home/gallery-b.jpg" alt="">
+          </div>
 
-              <img class="h-36 md:h-72 w-full object-cover object-left rounded-lg shadow-xl" src="/images/home/gallery-c.jpg" alt="">
+          <!-- Picture 1 -->
+          <div class="col-start-2 row-span-3">
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full rounded-xl" src="/images/home/hero-b.jpg" alt="">
+          </div>
 
-            </div>
+          <!-- Picture 2 -->
+          <div class="col-start-1 row-span-3 bg-white">
+
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full rounded-xl" src="/images/home/hero-a.jpg" alt="">
+
+          </div>
+
+          <!-- Picture 3 -->
+           <div class="col-start-2 row-span-4 bg-white">
+
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full rounded-xl" src="/images/home/hero-c.jpg" alt="">
+
+          </div>
+
+           <!-- Picture 4 -->
+          <div class="col-start-1 row-span-3 bg-white">
+
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full rounded-xl" src="/images/home/hero-g.jpg" alt="">
+
+          </div>
+
+
 
         </div>
+
       </div>
-    </section>
-  </div>
+
+    </div>
+
+  </section>
 </template>
