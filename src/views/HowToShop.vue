4852164d488bd63cd4672525ebb73d1cabe48bb5
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-06-04 00:07:14 -0400

    Put header in fluid container

--- src/views/HowToShop.vue
+++ src/views/HowToShop.vue
@@ -1,10 +1,11 @@
 <template>
   <section>
+
     <!-- Background fluid container -->
-    <div class="bg-white">
+    <div class="bg-gray-100">
 
       <!-- Content container -->
-      <div class="py-5 md:py-10 px-4 md:px-12 xl:px-20 2xl:container">
+      <div class="py-5 md:py-10 px-4 md:px-12 xl:px-32 2xl:container">
 
         <content-page-heading>
           <template v-slot:header>
@@ -15,12 +16,16 @@
           </template>
         </content-page-heading>
 
-        <!--
-        <div class="grid grid-cols-2 gap-5">
-          <img class="col-span-1 h-96  object-cover w-full" src="https://via.placeholder.com/950x400" alt="" />
-          <img class="col-span-1 object-cover h-96 w-full" src="https://via.placeholder.com/950x300" alt="" />
-        </div>
-        -->
+      </div>
+
+    </div>
+
+
+    <!-- Background fluid container -->
+    <div class="bg-white">
+
+      <!-- Content container -->
+      <div class="py-5 md:py-10 px-4 md:px-12 xl:px-32 2xl:container">
 
         <!-- Content box -->
         <div class="py-20">
