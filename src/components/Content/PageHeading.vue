185ae0c3fba84e778eb98a6ad22331b3e080a856
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-06-06 16:28:30 -0400

    Update styles

--- src/components/Content/PageHeading.vue
+++ src/components/Content/PageHeading.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="max-w-prose mt-20 mb-20">
+  <!-- Background fluid container -->
+  <div class="bg-gray-100">
 
-    <h1 class="text-4xl sm:text-6xl lg:text-7xl xl:text-7xl text-gray-900 font-medium">
-      <slot name="header">No heading provided</slot>
-    </h1>
+    <!-- Content container -->
+    <div class="py-5 md:py-10 px-4 md:px-12 xl:px-32 2xl:container">
 
-    <div class="my-10 h-2 w-16 bg-black"></div>
+      <div class="max-w-prose mt-20 mb-20">
 
-    <p class="mt-5 text-2xl md:text-2xl text-gray-600 lesading-loose">
-      <slot name="description">No description provided</slot>
-    </p>
+        <h1 class="text-4xl sm:text-6xl lg:text-7xl xl:text-7xl text-gray-900 font-semibold">
+          <slot name="header">No heading provided</slot>
+        </h1>
+
+        <div class="my-10 h-1 w-16 bg-black"></div>
+
+        <p class="mt-5 text-2xl md:text-2xl text-gray-600 lesading-loose">
+          <slot name="description">No description provided</slot>
+        </p>
+
+      </div>
+
+    </div>
 
   </div>
 </template>
