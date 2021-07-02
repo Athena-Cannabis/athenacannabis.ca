73cacb2db0578a194eff57efd742393faed144f9
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-07-02 09:52:07 -0400

    Revert "Add new hours"
    This reverts commit a708afaf98a6267f27b60f8165403eeb5eda8109.

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -44,15 +44,6 @@
 
                 <dl class="mt-4 text-gray-400 text-xl">
 
-                  <div class="pt-2 sm:pt-0 grid grid-cols-5 gap-4 text-gray-300">
-                    <dt class="col-span-2">
-                      Canada Day
-                    </dt>
-                    <dd class="col-span-3 text-right md:text-left">
-                      <span class="whitespace-nowrap">9:00 AM - 2:00 PM</span>
-                    </dd>
-                  </div>
-
                   <div class="pt-2 sm:pt-0 grid grid-cols-5 gap-4">
                     <dt class="col-span-2">
                       Monday - Thursday
